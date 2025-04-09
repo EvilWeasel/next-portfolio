d5ef79a7f58a62e59c7da0a5561ae80fc61350ce
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2025-04-09 18:37:56 +0200

    fu tailwind4

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+
+export const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight />
+        <Spotlight />
+        <Spotlight />
+      </div>
+    </div>
+  );
+};
