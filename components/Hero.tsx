eabd84fa28de37e6c950544e1ce70c03aee4f140
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2025-04-11 00:42:48 +0200

    fixed hecking tailwindcss4, migrated to v3; implemented simple hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { GridBackground } from "./ui/GridBackground";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 export const Hero = () => {
   return (
     <div className="pb-20 pt-36">
       <div>
-        <Spotlight />
-        <Spotlight />
-        <Spotlight />
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
+      <GridBackground>
+        <div className="max-w-[90vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </h2>
+          <TextGenerateEffect
+            duration={2}
+            words="How much wood could a woodchuck chuck, if a woodchuck could chuck wood?"
+          />
+        </div>
+      </GridBackground>
     </div>
   );
 };
